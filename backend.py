559d4e275ba6234e90d5aa8e002391b9fb318dd9
Donny Kurniawan <donny.kurniawan@gmail.com>
2016-05-20 08:26:55 +1000

    Test Redisco

--- backend.py
+++ backend.py
@@ -8,6 +8,9 @@ import json
 import uuid
 from flask import Flask, render_template
 from flask_sockets import Sockets
+import redisco
+from redisco import models
+import urlparse
 
 REDIS_URL = os.environ['REDIS_URL']
 REDIS_CHAN = 'channel'
@@ -27,6 +30,8 @@ class BackEnd(object):
         self.clients = list()
         self.pubsub = redis.pubsub()
         self.pubsub.subscribe(REDIS_CHAN)
+        redis_url = urlparse.urlparse(REDIS_URL)
+        redisco.connection_setup(host=redis_url.hostname, port=redis_url.port, password=redis_url.password)
 
     def __iter_data(self):
         for message in self.pubsub.listen():
@@ -57,8 +62,11 @@ class BackEnd(object):
         """Maintains Redis subscription in the background."""
         gevent.spawn(self.run)
 
+
+
 backend = BackEnd()
 backend.start()
+
 
 
 @app.route('/')
@@ -74,23 +82,11 @@ def inbox(ws):
         raw_message = ws.receive()
 
         if raw_message:
-            board_lists = {}
-
             app.logger.info(u'Inserting message: {}'.format(raw_message))
             message = json.loads(raw_message)
-            if message['REQ'] == 'NEWLIST':
-                board_lists = {'RESP': 'NEWLIST', 'IDENTIFIER': str(uuid.uuid4()), 'TEXT': 'TEMP Untitled'}
-            elif message['REQ'] == 'RENAMELIST':
-                board_lists = {'RESP': 'RENAMELIST', 'IDENTIFIER': message['IDENTIFIER'], 'TEXT': message['TEXT']}
-            elif message['REQ'] == 'NEWCARD':
-                board_lists = {'RESP': 'NEWCARD', 'IDENTIFIER': str(uuid.uuid4()), 'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'TEXT': 'TEMPCARD Untitled'}
-            elif message['REQ'] == 'RENAMECARD':
-                board_lists = {'RESP': 'RENAMECARD', 'IDENTIFIER': message['IDENTIFIER'], 'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'TEXT': message['TEXT']}
-            elif message['REQ'] == 'UPVOTECARD':
-                board_lists = {'RESP': 'UPVOTECARD', 'IDENTIFIER': message['IDENTIFIER'], 'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'COUNTER': 9}
-
-
-            redis.publish(REDIS_CHAN, json.dumps(board_lists))
+            result = process_message(message)
+            if result is not None:
+                redis.publish(REDIS_CHAN, json.dumps(result))
 
 @sockets.route('/receive')
 def outbox(ws):
@@ -100,3 +96,33 @@ def outbox(ws):
     while not ws.closed:
         # Context switch while `BackEnd.start` is running in the background.
         gevent.sleep(0.1)
+
+
+
+class Card(models.Model):
+    identifier = models.Attribute()
+    text = models.Attribute()
+    counter = models.Counter()
+
+class CardList(models.Model):
+    identifier = models.Attribute()
+    text = models.Attribute()
+    cards = models.ListField(Card)
+
+def process_message(message):
+    result = None
+
+    if message['REQ'] == 'NEWLIST':
+        card_list = CardList(identifier=str(uuid.uuid4()), text="Untitled")
+        card_list.save()
+        result = {'RESP': 'NEWLIST', 'IDENTIFIER': card_list.identifier, 'TEXT': card_list.text}
+    elif message['REQ'] == 'RENAMELIST':
+        result = {'RESP': 'RENAMELIST', 'IDENTIFIER': message['IDENTIFIER'], 'TEXT': message['TEXT']}
+    elif message['REQ'] == 'NEWCARD':
+        result = {'RESP': 'NEWCARD', 'IDENTIFIER': str(uuid.uuid4()), 'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'TEXT': 'TEMPCARD Untitled'}
+    elif message['REQ'] == 'RENAMECARD':
+        result = {'RESP': 'RENAMECARD', 'IDENTIFIER': message['IDENTIFIER'], 'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'TEXT': message['TEXT']}
+    elif message['REQ'] == 'UPVOTECARD':
+        result = {'RESP': 'UPVOTECARD', 'IDENTIFIER': message['IDENTIFIER'], 'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'COUNTER': 9}
+
+    return result
