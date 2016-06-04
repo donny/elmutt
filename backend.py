8b46aabf21a32d65b17581a1b60aa0ab2067f7df
Donny Kurniawan <donny.kurniawan@gmail.com>
2016-06-04 19:20:03 +1000

    Beautify the Python code

--- backend.py
+++ backend.py
@@ -16,11 +16,10 @@ REDIS_URL = os.environ['REDIS_URL']
 REDIS_CHAN = 'channel'
 
 app = Flask(__name__)
-app.debug = True #'DEBUG' in os.environ
+app.debug = True  # 'DEBUG' in os.environ
 
 sockets = Sockets(app)
 redis = redis.from_url(REDIS_URL)
-
 
 
 class BackEnd(object):
@@ -31,7 +30,8 @@ class BackEnd(object):
         self.pubsub = redis.pubsub()
         self.pubsub.subscribe(REDIS_CHAN)
         redis_url = urlparse.urlparse(REDIS_URL)
-        redisco.connection_setup(host=redis_url.hostname, port=redis_url.port, password=redis_url.password)
+        redisco.connection_setup(
+            host=redis_url.hostname, port=redis_url.port, password=redis_url.password)
 
     def __iter_data(self):
         for message in self.pubsub.listen():
@@ -63,15 +63,14 @@ class BackEnd(object):
         gevent.spawn(self.run)
 
 
-
 backend = BackEnd()
 backend.start()
-
 
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
 
 @sockets.route('/submit')
 def inbox(ws):
@@ -88,6 +87,7 @@ def inbox(ws):
             if result is not None:
                 redis.publish(REDIS_CHAN, json.dumps(result))
 
+
 @sockets.route('/receive')
 def outbox(ws):
     """Sends outgoing messages, via `BackEnd`."""
@@ -98,16 +98,17 @@ def outbox(ws):
         gevent.sleep(0.1)
 
 
-
 class Card(models.Model):
     identifier = models.Attribute()
     text = models.Attribute()
     counter = models.Counter()
 
+
 class CardList(models.Model):
     identifier = models.Attribute()
     text = models.Attribute()
     cards = models.ListField(Card)
+
 
 def process_message(message):
     result = None
@@ -116,36 +117,45 @@ def process_message(message):
         raw_card_lists = CardList.objects.all()
         all_card_lists = []
         for raw_card_list in raw_card_lists:
-            card_list = {'text': raw_card_list.text, 'identifier': raw_card_list.identifier, 'cards': []}
+            card_list = {'text': raw_card_list.text,
+                         'identifier': raw_card_list.identifier, 'cards': []}
             for raw_card in raw_card_list.cards:
-                card_list['cards'].append({'text': raw_card.text, 'identifier': raw_card.identifier, 'counter': raw_card.counter})
+                card_list['cards'].append(
+                    {'text': raw_card.text, 'identifier': raw_card.identifier, 'counter': raw_card.counter})
             all_card_lists.append(card_list)
         result = {'RESP': 'RESP_REFRESH', 'DATA': all_card_lists}
     elif message['REQ'] == 'NEWLIST':
         card_list = CardList(identifier=str(uuid.uuid4()), text="Untitled")
         card_list.save()
-        result = {'RESP': 'RESP_NEWLIST', 'IDENTIFIER': card_list.identifier, 'TEXT': card_list.text}
+        result = {'RESP': 'RESP_NEWLIST',
+                  'IDENTIFIER': card_list.identifier, 'TEXT': card_list.text}
     elif message['REQ'] == 'RENAMELIST':
-        card_list = CardList.objects.filter(identifier=message['IDENTIFIER']).first()
+        card_list = CardList.objects.filter(
+            identifier=message['IDENTIFIER']).first()
         card_list.text = message['TEXT']
         card_list.save()
-        result = {'RESP': 'RESP_RENAMELIST', 'IDENTIFIER': card_list.identifier, 'TEXT': card_list.text}
+        result = {'RESP': 'RESP_RENAMELIST',
+                  'IDENTIFIER': card_list.identifier, 'TEXT': card_list.text}
     elif message['REQ'] == 'NEWCARD':
         card = Card(identifier=str(uuid.uuid4()), text="Untitled")
         card.save()
-        card_list = CardList.objects.filter(identifier=message['LISTIDENTIFIER']).first()
+        card_list = CardList.objects.filter(
+            identifier=message['LISTIDENTIFIER']).first()
         card_list.cards.append(card)
         card_list.save()
-        result = {'RESP': 'RESP_NEWCARD', 'IDENTIFIER': card.identifier, 'LISTIDENTIFIER': card_list.identifier, 'TEXT': card.text}
+        result = {'RESP': 'RESP_NEWCARD', 'IDENTIFIER': card.identifier,
+                  'LISTIDENTIFIER': card_list.identifier, 'TEXT': card.text}
     elif message['REQ'] == 'RENAMECARD':
         card = Card.objects.filter(identifier=message['IDENTIFIER']).first()
         card.text = message['TEXT']
         card.save()
-        result = {'RESP': 'RESP_RENAMECARD', 'IDENTIFIER': card.identifier, 'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'TEXT': card.text}
+        result = {'RESP': 'RESP_RENAMECARD', 'IDENTIFIER': card.identifier,
+                  'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'TEXT': card.text}
     elif message['REQ'] == 'UPVOTECARD':
         card = Card.objects.filter(identifier=message['IDENTIFIER']).first()
         card.incr('counter')
         card.save()
-        result = {'RESP': 'RESP_UPVOTECARD', 'IDENTIFIER': card.identifier, 'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'COUNTER': card.counter}
+        result = {'RESP': 'RESP_UPVOTECARD', 'IDENTIFIER': card.identifier,
+                  'LISTIDENTIFIER': message['LISTIDENTIFIER'], 'COUNTER': card.counter}
 
     return result
